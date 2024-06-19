355b22c066923da1e02ecbad544a894e50da9e4b
Kevin-Duoc <k.fuenzalida@duocuc.cl>
2024-06-18 22:52:41 -0400

    Add files via upload

---
+++ 2.7.py
@@ -0,0 +1,33 @@
+#pregunta 11
+x = 1
+y = 2
+z = x
+x = y
+y = z
+print(x, y)
+
+#pregunta 15
+x = 11
+y = 4
+ 
+x = x % y
+x = x % y
+y = y % x
+ 
+print(y)
+
+#pregunta 16
+x = "3"
+y = 6
+ 
+print(x * y)
+
+#pregunta 17
+z = y = x = 1
+print(x, y, z, sep='*')
+
+#pregunta 19
+x = 1 / 2 + 3 // 3 + 4 ** 2
+print(x)
+
+print(20.12 * 10 ** 8)
